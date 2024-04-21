6cbdfabd483f816b97f07449ee297e1acfabf345
Douglas Paz <eu@douglaspaz.com>
2024-04-21 00:14:20 -0300

    feat: add style support

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
-const { EnvironmentPlugin, DefinePlugin } = require('webpack');
+const { DefinePlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const PRODUCTION = 'production';
 const DEVELOPMENT = 'development';
 const isProduction = process.env.NODE_ENV === PRODUCTION;
+const stylesHandler = MiniCssExtractPlugin.loader;
 
 const config = {
   entry: './src/index.js',
@@ -19,12 +21,21 @@ const config = {
     new HtmlWebpackPlugin({
         template: 'index.html',
     }),
+    new MiniCssExtractPlugin(),
     new DefinePlugin({
       __VUE_OPTIONS_API__: true,
       __VUE_PROD_DEVTOOLS__: !isProduction,
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: !isProduction,
     }),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, 'css-loader', 'sass-loader'],
+      }
+    ]
+  }
 };
 
 module.exports = () => {
