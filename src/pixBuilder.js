c9839bdabff6597341c0ef3d1c485f04b18d3088
Douglas Paz <eu@douglaspaz.com>
2024-04-24 09:49:34 -0300

    feat: init pix builder

---
+++ src/pixBuilder.js
@@ -0,0 +1,14 @@
+import { Pix } from 'faz-um-pix/lib'
+
+export default async (pixKey, pixKeyOwnerName, pixKeyOwnerCity, ticketPrice, message) => {
+  const pixArgs = [
+    pixKey,
+    pixKeyOwnerName,
+    pixKeyOwnerCity,
+    ticketPrice.toFixed(2),
+    message.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+  ]
+  const pixURL = await Pix(...pixArgs)
+  const pixQrCode = await Pix(...pixArgs, true)
+  return { pixURL, pixQrCode }
+};
